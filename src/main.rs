624951e7b46e780f166af84237bcb17a6132404f
Pedro Mendonca <peumendonca@gmail.com>
2017-02-25 12:53:05 +0000

    Added web server

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
-use std::net::{TcpListener, TcpStream}
-fn main() {
+use std::net::{TcpListener, TcpStream};
+
+fn main() 
+{
+    #![allow(non_snake_case)]
     println!("Hello, world!");
-    startWeb();
+    start_web();
 }
 
-fn startWeb() {
-    
+fn start_web() 
+{
+    let listener = TcpListener::bind("127.0.0.1:2222").unwrap();
+    //accept connections and processing them
+    for stream in listener.incoming()
+    {
+        match stream
+        {
+            Ok(stream) => handle_client(stream),
+            Err(_) => println!("Connection failed")
+        }
+    }
+
+}
+fn handle_client(stream:TcpStream) 
+{
+    println!("Connection handled");
+    //
 }
