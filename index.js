bddf6110291047f77cb1a9878138c4360841d1d2
Pedro Mendonca <peumendonca@gmail.com>
2017-02-26 05:39:56 +0000

    Code based from the whoel of Rust yeah its done in like 20 min

--- index.js
+++ index.js
@@ -8,11 +8,22 @@ app.use(express.static('static'))
 //opening port 8000 for http
 app.listen(8000, function()
 {
+    var greeting = "________        .__        __                                      \r\n\\_____  \\  __ __|__| ____ |  | __  ______ ______________  __ ____  \r\n \/  \/ \\  \\|  |  \\  |\/ ___\\|  |\/ \/ \/  ___\/\/ __ \\_  __ \\  \\\/ \/\/ __ \\ \r\n\/   \\_\/.  \\  |  \/  \\  \\___|    <  \\___ \\\\  ___\/|  | \\\/\\   \/\\  ___\/ \r\n\\_____\\ \\_\/____\/|__|\\___  >__|_ \\\/____  >\\___  >__|    \\_\/  \\___  >\r\n       \\__>             \\\/     \\\/     \\\/     \\\/                 \\\/ ";
+    console.log(greeting)
     console.log("Listening on port 8000")
 })
 
 
 //routes
+app.get("/s/*", function(req,res)
+{
+    //handle the creation of HTTP requests
+    var method = req.method;
+    var path = req.url.substring(3, req.url.length)
+
+    res.send(method + " " + path)
+})
+
 app.get("/", function(req,res)
 {
     res.render('index')
