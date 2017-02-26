457c10dee2e18285f28aafd6070e28fbe978ef5d
Pedro Mendonca <peumendonca@gmail.com>
2017-02-26 07:10:04 +0000

    Routing working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express'),
-      app = express()
+    app = express(),
+    request = require('request')
 
 //settings
 app.set('view engine', 'pug')
@@ -7,30 +8,37 @@ app.use(express.static('static'))
 
 //opening port 8000 for http
 app.listen(8000, function()
-{
+    {
     var greeting = " ________        .__        __                                      \r\n\\_____  \\  __ __|__| ____ |  | __  ______ ______________  __ ____  \r\n \/  \/ \\  \\|  |  \\  |\/ ___\\|  |\/ \/ \/  ___\/\/ __ \\_  __ \\  \\\/ \/\/ __ \\ \r\n\/   \\_\/.  \\  |  \/  \\  \\___|    <  \\___ \\\\  ___\/|  | \\\/\\   \/\\  ___\/ \r\n\\_____\\ \\_\/____\/|__|\\___  >__|_ \\\/____  >\\___  >__|    \\_\/  \\___  >\r\n       \\__>             \\\/     \\\/     \\\/     \\\/                 \\\/ "
     console.log(greeting)
     console.log("Listening on port 8000")
-})
+    })
 
 
 //routes
 app.get("/s/*", function(req,res)
-{
+    {
     //handle the creation of HTTP requests
     var method = req.method;
     var path = req.url.substring(3, req.url.length)
     var slug = path.substring(0, path.indexOf("/"))
-    var path = path.substring(path.indexOf("/")+1, path.length)
-    console.log(req)
+    var path = "/"+path.substring(path.indexOf("/")+1, path.length)
+    //making the request
+    request('http://localhost:'+slug+path, function(response,body) {
+        // console.log(response.statusCode) // 200
+        // console.log(response.headers['content-type']) // 'image/png'
+        // res.send(response)
+        // console.log(reponse)
+        console.log(body)
+        res.send(body.body)
+    })
 
-    res.send(method + " at " + slug + " in " + path + "<br/>Request: " + makeHTTP(method,path) )
 })
 
 app.get("/", function(req,res)
-{
+    {
     res.render('index')
-})
+    })
 
 function makeHTTP(method, path)
 {
