58927c16af0515af4655f576c329e595d99c625a
Jonathan Balls <j.balls@btopenworld.com>
2017-02-26 06:15:17 +0000

    Trying to accept ssh

--- ssh.js
+++ ssh.js
@@ -5,33 +5,41 @@ var inspect = require('util').inspect;
 var ssh2 = require('ssh2');
 var utils = ssh2.utils;
 
-new ssh2.Server({
-  hostKeys: []
-}, function(client) {
-  console.log('Client connected!');
+function main()
+{
+    new ssh2.Server({
+      hostKeys: [fs.readFileSync('host.key')]
+    }, function(client) {
+      console.log('Client connected!');
 
-  client.on('authentication', function(ctx) {
-    if (ctx.method === 'password'
-        && ctx.username === 'foo'
-        && ctx.password === 'bar')
-      ctx.accept();
-  }).on('ready', function() {
-    console.log('Client authenticated!');
+      client.on('authentication', function(ctx) {
+          ctx.accept();
+      }).on('ready', function() {
 
-    client.on('session', function(accept, reject) {
-      var session = accept();
-      session.once('exec', function(accept, reject, info) {
-        console.log('Client wants to execute: ' + inspect(info.command));
-        var stream = accept();
-        stream.stderr.write('Oh no, the dreaded errors!\n');
-        stream.write('Just kidding about the errors!\n');
-        stream.exit(0);
-        stream.end();
+        client.on('session', function(accept, reject) {
+          var session = accept();
+          session.write("hello");
+          //session.once('exec', function(accept, reject, info) {
+            //console.log('Client wants to execute: ' + inspect(info.command));
+            //var stream = accept();
+            //stream.stderr.write('Oh no, the dreaded errors!\n');
+            //stream.write('Just kidding about the errors!\n');
+            //stream.exit(0);
+            //stream.end();
+          //});
+
+          session.once('forwardOut', function(accept, reject, info) {
+            console.log("Creating port forwarding");
+            var stream = accept();
+
+            return true;
+          });
+        });
+      }).on('end', function() {
+        console.log('Client disconnected');
       });
+    }).listen(2222, '127.0.0.1', function() {
+      console.log('Listening on port ' + this.address().port);
     });
-  }).on('end', function() {
-    console.log('Client disconnected');
-  });
-}).listen(0, '127.0.0.1', function() {
-  console.log('Listening on port ' + this.address().port);
-});
+}
+main();
